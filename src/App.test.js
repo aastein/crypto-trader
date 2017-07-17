fd7d00546c9fa1f751de248c77f98812cae3d4f2
Aaron Stein <aaronstein12@gmail.com>
2017-07-17 12:01:17 -0700

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
+import reducer from './reducers';
 
 it('renders without crashing', () => {
+  const store = createStore(reducer);
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  , div);
 });
