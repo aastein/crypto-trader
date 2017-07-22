d868933c245bd083358a2f9e4c74e942ebb748c0
Aaron Stein <aaronstein12@gmail.com>
2017-07-21 21:27:04 -0700

    line chart proptypes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,28 @@
 
 import React from 'react';
 import Spinner from 'react-spinkit';
+import PropTypes from 'prop-types';
 
-const Loader = () => (
+const Loader = ({ className, color, fadeIn, name }) => (
   (
-    <div className="spinner">
-      <Spinner name="ball-clip-rotate-multiple" fadeIn="none" color="#007eff" />
+    <div className={className}>
+      <Spinner color={color} fadeIn={fadeIn} name={name} />
     </div>
   )
 );
+
+Loader.propTypes = {
+  className: PropTypes.string,
+  name: PropTypes.string,
+  fadeIn: PropTypes.string,
+  color: PropTypes.string,
+};
+
+Loader.defaultProps = {
+  className: 'spinner',
+  color: '#007eff',
+  fadeIn: 'none',
+  name: 'ball-clip-rotate-multiple',
+};
 
 export default Loader;
