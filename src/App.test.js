51ef9fb0dfba43fd9b07b08854de6422ac7eff33
Aaron Stein <aaronstein12@gmail.com>
2017-07-23 01:52:52 -0700

    dummy test

--- src/App.test.js
+++ src/App.test.js
@@ -5,8 +5,5 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-import App from './App';
-import reducer from './reducers';
-
 it('is a dummy test', () => {
 });
